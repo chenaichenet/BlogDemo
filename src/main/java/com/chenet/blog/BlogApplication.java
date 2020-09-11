bbfa68d5dfb437b667e2cf2d57a43c727a8e1f65
十七 <chenaichenet@126.com>
2020-09-11 08:59:29 +0800

    主启动

---
+++ src/main/java/com/chenet/blog/BlogApplication.java
@@ -0,0 +1,17 @@
+/*
+  FileName: BlogApplication
+  Author:   嘉平十七
+  Date:     2020/8/19 9:55
+  Description:
+ */
+package com.chenet.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class,args);
+    }
+}
