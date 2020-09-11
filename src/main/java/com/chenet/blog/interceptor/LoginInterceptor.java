e0086da21fd8b71ba3e3a4198469601feb5da586
十七 <chenaichenet@126.com>
2020-09-11 08:58:56 +0800

    拦截过滤

---
+++ src/main/java/com/chenet/blog/interceptor/LoginInterceptor.java
@@ -0,0 +1,25 @@
+/**
+ * FileName: LoginInterceptor
+ * Author:   嘉平十七
+ * Date:     2020/8/19 14:21
+ * Description: 登录过滤
+ */
+package com.chenet.blog.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        if (request.getSession().getAttribute("user")==null){
+            response.sendRedirect("/admin");
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/chenet/blog/interceptor/WebConfig.java
@@ -0,0 +1,22 @@
+/**
+ * FileName: WebConfig
+ * Author:   嘉平十七
+ * Date:     2020/8/19 14:24
+ * Description: 拦截配置
+ */
+package com.chenet.blog.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin")
+                .excludePathPatterns("/admin/login");
+    }
+}
