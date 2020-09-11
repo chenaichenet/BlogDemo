d4981246105bed5fb9f2d61c5e26d576299db51a
十七 <chenaichenet@126.com>
2020-09-11 08:56:58 +0800

    工具类

---
+++ src/main/java/com/chenet/blog/util/NotFoundException.java
@@ -0,0 +1,24 @@
+/**
+ * FileName: NotFoundException
+ * Author:   嘉平十七
+ * Date:     2020/8/19 10:15
+ * Description: 异常类
+ */
+package com.chenet.blog.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
